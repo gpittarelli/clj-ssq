07f4ef742fcc4005b7b958d34e90fc2cd612a2b0
George Pittarelli <gjp@cs.umd.edu>
2015-08-09 14:05:23 -0400

    tests with dummy ssq server for each request type

--- test/clj_ssq/core_test.clj
+++ test/clj_ssq/core_test.clj
@@ -1,7 +1,60 @@
 (ns clj-ssq.core-test
   (:require [clojure.test :refer :all]
-            [clj-ssq.core :refer :all]))
+            [clj-ssq.core :refer :all]
+            [clj-ssq.core-test-data :refer :all])
+  (:import [java.net DatagramSocket DatagramPacket]
+           [java.io ByteArrayInputStream ByteArrayOutputStream]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def ^:dynamic *test-server* "127.0.0.1")
+(def ^:dynamic *test-port* -1)
+
+(defn dummy-ssq-server-fixture [run-tests]
+  (with-open [server-socket (DatagramSocket. 9876)]
+    (let [recv-buf (byte-array 1000)
+          recv-packet (DatagramPacket. recv-buf 1000)]
+      (future
+        (while (not (.isClosed server-socket))
+          (when (try
+                  (.receive server-socket recv-packet)
+                  true
+                  (catch Exception err
+                    ;; (println "Test server error" err)
+                    false))
+            (let [query (take (.getLength recv-packet) recv-buf)
+                  response
+                  (cond ;; note: case doesn't work nicely with byte
+                        ;; arrays, have to use cond
+                    (= query (seq info-query))
+                    info-response
+
+                    (= query (seq player-challenge-query))
+                    player-challenge-response
+                    (= query (seq player-query))
+                    player-response
+
+                    (= query (seq rules-challenge-query))
+                    rules-challenge-response
+                    (= query (seq rules-query))
+                    rules-response
+
+                    :else (byte-array [1 1 1]))
+
+                  responses
+                  (if (sequential? response)
+                    response
+                    [response])]
+              (doseq [datagram responses]
+                (.send
+                 server-socket
+                 (DatagramPacket. datagram (count datagram)
+                                  (.getSocketAddress recv-packet)))))))))
+
+    (binding [*test-port* (.getLocalPort server-socket)]
+      (run-tests))))
+
+(use-fixtures :once dummy-ssq-server-fixture)
+
+(deftest query-test
+  (is (= @(info *test-server* *test-port*) info-response-value))
+  (is (= @(rules *test-server* *test-port*) rules-response-value))
+  (is (= @(players *test-server* *test-port*) player-response-value)))
